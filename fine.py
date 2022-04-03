0c6e5b9d3bc132fb123c579727d56cc50a0ae8a2
dennis-ming <93405721+dennis-ming@users.noreply.github.com>
2022-04-03 22:58:35 +0900

    changing fine.py adding print output name

--- fine.py
+++ fine.py
@@ -2,6 +2,9 @@
 from google.protobuf.json_format import MessageToDict
 import onnx
 
-model = onnx.load("./mnist.onnx")
+model = onnx.load("./model.onnx")
 for _input in model.graph.input:
     print(MessageToDict(_input))
+
+for _output in model.graph.output:
+    print(MessageToDict(_output))
