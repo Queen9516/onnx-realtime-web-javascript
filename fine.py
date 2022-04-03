66135e738327e74ab8e73ad217050beb5c6d564c
dennis-ming <93405721+dennis-ming@users.noreply.github.com>
2022-04-04 00:08:42 +0900

    adding fine.py

--- fine.py
+++ fine.py
@@ -2,7 +2,7 @@
 from google.protobuf.json_format import MessageToDict
 import onnx
 
-model = onnx.load("./model.onnx")
+model = onnx.load("./mnist.onnx")
 for _input in model.graph.input:
     print(MessageToDict(_input))
 
